25207dec2e171bd2ddb8c82addbf346a128f320f
PitchBlackNights <79918522+PitchBlackNights@users.noreply.github.com>
2022-05-04 17:24:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ echo "\e[1;33mTo Add Saving Directories, please run this command \"nano $HOME/Li
 echo "Then add new entries to the tables \"cfgNames\" & \"cfgPaths\"\e[0m"
 echo
 echo "Note: Somtimes when you take a screenshot, the screen can bug out, and flash black. To fix this, just drag your cursor over the flashing parts, and retake the screenshot"
+echo
+echo "To take a screenshot, run the \"Screenshot.sh\" file on your desktop (you can move the file anywhere)"
