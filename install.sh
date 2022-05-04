1bb1787a33bfcf73641a2a4541cd816acae64372
PitchBlackNights <79918522+PitchBlackNights@users.noreply.github.com>
2022-05-04 17:21:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo rm -rf $HOME/Linux-Screenshot-Tool/.git
 
 clear
 
-echo -e "\e[1;33mTo Add Saving Directories, please run this command \"nano $HOME/Linux-Screenshot-Tool/src/config.py\""
-echo -e "Then add new entries to the tables \"cfgNames\" & \"cfgPaths\"\e[0m"
+echo "\e[1;33mTo Add Saving Directories, please run this command \"nano $HOME/Linux-Screenshot-Tool/src/config.py\""
+echo "Then add new entries to the tables \"cfgNames\" & \"cfgPaths\"\e[0m"
 echo
-echo -e "Note: Somtimes when you take a screenshot, the screen can bug out, and flash black. To fix this, just drag your cursor over the flashing parts, and retake the screenshot"
+echo "Note: Somtimes when you take a screenshot, the screen can bug out, and flash black. To fix this, just drag your cursor over the flashing parts, and retake the screenshot"
