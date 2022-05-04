35fe421692b13a6c71a522f67554d6de0be7d6d3
PitchBlackNights <79918522+PitchBlackNights@users.noreply.github.com>
2022-05-04 17:13:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ clear
 
 echo -e "\e[1;33mTo Add Saving Directories, please run this command \"nano $HOME/Linux-Screenshot-Tool/src/config.py\""
 echo -e "Then add new entries to the tables \"cfgNames\" & \"cfgPaths\"\e[0m"
+echo
+echo -e "Note: Somtimes when you take a screenshot, the screen can bug out, and flash black. To fix this, just drag your cursor over the flashing parts, and retake the screenshot"
